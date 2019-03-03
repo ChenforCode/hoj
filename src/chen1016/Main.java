478390b007f73104f2bc641e28b40a6663642026
ChenforCode <727949427@qq.com>
2019-03-03 23:42:01 +0800

    fix some bugs of hoj1016

--- src/chen1016/Main.java
+++ src/chen1016/Main.java
@@ -8,85 +8,72 @@ import java.util.Scanner;
  * @description: 一个求排列的问题，所以用DFS+回溯即可
  */
 public class Main {
-    /*static int[] a;
     static int[] book;
-    static int num;
-    static int ans = 1;
+    static int[] a;
+    static int n;
+    static boolean haveResult = false;
+    static int resultNum = 1;
+    static int caseNum = 1;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while ((num = sc.nextInt()) > 0 && num < 20) {
-            //对数据进行初始化
-            a = new int[num + 1];
-            book = new int[num + 1];
+        while (true) {
+            n = sc.nextInt();
+            a = new int[n + 1];
+            book = new int[n + 1];
             a[1] = 1;
             book[1] = 1;
-            if (ans != 1){
-                System.out.println();
-            }
-            System.out.println("Case " + ans + ":");
+            haveResult = false;
+            resultNum = 1;
             dfs(2);
-            ans++;
+            if (!haveResult){
+                break;
+            }
+            caseNum++;
         }
+
     }
 
     public static void dfs(int step) {
-        if (step == num + 1) {
-            //除了头尾，已经保证为素数
-            if (checkPrime(a[1] + a[num])) {
-                for (int i = 1; i <= num; i++) {
-                    if (i == num){
-                        System.out.print(a[i]);
-                    } else {
-                        System.out.print(a[i] + " ");
-                    }
+        if (step == n + 1 && checkPrime(a[1] + a[n])) {
+            haveResult = true;
+            if (resultNum == 1){
+                if (caseNum != 1) {
+                    System.out.println();
                 }
-                System.out.println();
+                System.out.println("Case " + caseNum + ":");
             }
+            for (int i = 1; i <= n; i++) {
+                if (i == n) {
+                    System.out.print(a[i]);
+                } else {
+                    System.out.print(a[i] + " ");
+                }
+            }
+            System.out.println();
+            resultNum++;
             return;
         }
-        //1已经被占用了，不需要判断了
-        for (int i = 2; i <= num; i++) {
-            //保证与前一个数字之和为素数
-            if (book[i] == 0 && checkPrime(i + a[step - 1])) {
-                a[step] = i;
+
+        for (int i = 2; i <= n; i++) {
+            if (book[i] == 0 && checkPrime(a[step - 1] + i)) {
                 book[i] = 1;
+                a[step] = i;
                 dfs(step + 1);
                 book[i] = 0;
             }
         }
     }
 
-    public static boolean checkPrime(int a) {
-        int k = (int) Math.sqrt(a);
-        for (int i = 2; i <= k; i++){
-            if (a % i == 0){
+    public static boolean checkPrime(int checkNum) {
+        if (checkNum < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(checkNum); i++) {
+            if (checkNum % i == 0) {
                 return false;
             }
         }
         return true;
-    }*/
-
-
-    //重新写吧。。判断素数的时候可以打表，因为数据量比较小
-    static int[] book = new int[20];
-    static int[] a;
-    static int n;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        a = new int[n + 1];
-
     }
-
-    public void dfs(int step){
-        if (step == n + 1 && a[1]){
-
-        }
-    }
-
-    public boolean checkPrime(){
-
-    }
-
 }
